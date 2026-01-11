fb0df023bed093b10f1a25154086d73e78fed4c8
Axeld <spb-webmaster@mail.ru>
2026-01-11 16:12:18 +0300

    11-yan-2026

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+
 Schedule::command('queue:work --stop-when-empty')
     ->everyMinute()
     ->withoutOverlapping();
